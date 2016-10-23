a50fe79209420a81d3f2f698a93d4d77abf08e3f
Frankie Gavin <frankanthonygavin@gmail.com>
2016-10-22 17:48:48 -0700

    add corrected front and back end labeling

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
-// This is the // This is the business (or back-end) logic:
+// This is the frontend logic
 
 
 $(document).ready(function(){
   $("form#choices").submit(function(event) {
-    var front = $("select#frontEnd").val();
-    var back = $("select#backEnd").val();
-    var mobile = $("select#mobile").val();
-    var large = ($("select#largeCompany").val());
-    var interactive = $("select#interactive").val();
-    $("#result").text(choice);
-    $("#hidden").fadeIn();
+    event.preventDefault()
+      var front = $("select#frontEnd").val();
+      var back = $("select#backEnd").val();
+      var mobile = $("select#mobile").val();
+      var large = ($("select#largeCompany").val());
+      var interactive = $("select#interactive").val();
+
+
 // This is the business (or back-end) logic:
 
-    var choice = function(){
+var choice = function(){
 
-        if (front === "Yes") {
-        return ("CSS/Design");
-        }else if (back === "Yes") {
-        return ("PHP/Drupal");
-      } else if (mobile === "Yes") {
-        return ("Java/Android");
-      } else if (large === "Yes") {
-        return ("C#/.NET");
-      } else if (interactive === "Yes") {
-        return ("Ruby/Rails");
-      }
-    };
-
-
+  if (front === "Yes") {
+    return ("CSS/Design");
+  }else if (back === "Yes") {
+    return ("PHP/Drupal");
+  } else if (mobile === "Yes") {
+    return ("Java/Android");
+  } else if (large === "Yes") {
+    return ("C#/.NET");
+  } else if (interactive === "Yes") {
+    return ("Ruby/Rails");
+  }
+};
 
 
 
+// This is also front-end logic:
 
-
-    event.preventDefault()
+    $("#result").text(choice);
+    $("#hidden").fadeIn();
   });
 });
